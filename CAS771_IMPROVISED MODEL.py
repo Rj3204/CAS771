a8b062dc7d33ef667bf9062ef80c229c649465c4
Rj3204 <104400786+Rj3204@users.noreply.github.com>
2022-04-26 00:15:03 -0400

    Update and rename CAS771_CIFAR10 (1).py to CAS771_IMPROVISED MODEL.py

--- CAS771_IMPROVISED MODEL.py
+++ CAS771_IMPROVISED MODEL.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-# test harness for evaluating models on the cifar10 dataset
 import sys
 import json
 from tensorflow import keras
@@ -22,38 +15,17 @@ from keras.layers import Dropout
 from keras.layers import BatchNormalization
 
 optimizer = keras.optimizers.SGD(learning_rate=0.001, momentum=0.9)  # replace "lr" with "learning_rate"
-
-# load train and test dataset
-
 def load_dataset():
-    # load dataset
     (trainX, trainY), (testX, testY) = cifar10.load_data(r'C:\Users\om\Dataset_CAS771\cifar10_noisy_labels_task1.json')
-    
-    # one hot encode target values
     trainY = to_categorical(trainY)
     testY = to_categorical(testY)
     return trainX, trainY, testX, testY
-
-
-# In[6]:
-
-
-# scale pixels
 def prep_pixels(train, test):
-    # convert from integers to floats
     train_norm = train.astype('float32')
     test_norm = test.astype('float32')
-    # normalize to range 0-1
     train_norm = train_norm / 255.0
     test_norm = test_norm / 255.0
-    # return normalized images
     return train_norm, test_norm
-
-
-# In[7]:
-
-
-# Improvised Model
 def define_model():
     model = Sequential()
     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
@@ -79,16 +51,9 @@ def define_model():
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
-    
     opt = SGD(learning_rate=0.001, momentum=0.9)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
     return model
-
-
-# In[4]:
-
-
-# plot diagnostic learning curves
 def summarize_diagnostics(history):
     # plot loss
     pyplot.subplot(211)
@@ -104,38 +69,15 @@ def summarize_diagnostics(history):
     filename = sys.argv[0].split('/')[-1]
     pyplot.savefig(filename + '_plot.png')
     pyplot.close()
-
-
-# In[ ]:
-
-
-# run the test harness for evaluating a model
 def run_test_harness():
-    # load dataset
     trainX, trainY, testX, testY = load_dataset()
-    # prepare pixel data
     trainX, testX = prep_pixels(trainX, testX)
-    # define model
     model = define_model()
-    # create data generator
     datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
-    # prepare iterator
     it_train = datagen.flow(trainX, trainY, batch_size=64)
-    # fit model
     steps = int(trainX.shape[0] / 64)
     history = model.fit(it_train, steps_per_epoch=steps, epochs=50, validation_data=(testX, testY), verbose=0)
-    # evaluate model
     _, acc = model.evaluate(testX, testY, verbose=0)
     print('> %.3f' % (acc * 100.0))
-    # learning curves
     summarize_diagnostics(history)
- 
-# entry point, run the test harness
 print(run_test_harness())  
-
-
-# In[ ]:
-
-
-
-
